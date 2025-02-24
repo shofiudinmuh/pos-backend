9a0407ec1d1c9e5a4c5777b66df66b16235a532f
shofiudinmuh <shofiudinmuh@gmail.com>
2025-02-24 17:50:18 +0700

    update : create category controller

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return response()->json(['message' => 'Data kategori berhasil diambil!', 'data' => $categories]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+        return response()->json(['message' => 'Categori berhasil disimpan!', 'data' => $category], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::findOrFail($id);
+        return response()->json(['message' => 'Detail kategori berhasil diambil!', 'data' => $category]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->update($request->all());
+        return response()->json(['message' => 'Kategori berhasil diupdate!', 'data' => $category]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Category::findOrFail($id)->delete();
+        return response()->json(['message' => 'Kategori berhasil dihapus!'], 204);
+    }
+}
