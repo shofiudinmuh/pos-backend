59ab693b3cb38fe125c06778e3a169a1cfbfdaa7
shofiudinmuh <shofiudinmuh@gmail.com>
2025-02-24 17:28:48 +0700

    update : create product controller

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $products = Product::all();
+
+        return response()->json(['message' => 'Data product berhasil diambil', 'data' => $products]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return response()->json(['message' => 'Data produt berhasil disimpan!', 'data' => $product], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::findOrFail($id);
+        return response()->json(['message' => 'Detail product berhasil diambil!', 'data' => $product]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+
+        return response()->json(['message' => 'Product berhasi diupdate!', 'data' => $product]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Product::findOrFail($id)->delete();
+        return response()->json(['message' => 'Product berhasil dihapus!'], 204);
+    }
+}
